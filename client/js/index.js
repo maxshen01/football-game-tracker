93b8f78e86bcd0482d7625ccbb63403d88562582
maxshen01 <maxshen2503@gmail.com>
2025-11-25 15:16:59 +0000

    add js to populate the table

--- client/js/index.js
+++ client/js/index.js
@@ -1,1 +1,61 @@
-console.log("hello world");
+let onLocalHost = true;
+let apiBeginning;
+
+if (onLocalHost) {
+    apiBeginning = "http://localhost:8080"
+} else {
+    apiBeginning = "http://"
+}
+
+
+const leagueTableHtml = document.querySelector(".league-table")
+
+document.addEventListener("DOMContentLoaded", fillLeagueTable)
+
+//fills the league table
+async function fillLeagueTable(e) {
+    //get the right object
+    
+
+    leagueTableList = await getLeagueTable()
+
+    //iterate through league table to get items
+    for (let i=0; i <leagueTableList.length; i++) {
+        const teamStats = leagueTableList[i]
+
+        const row = document.createElement("tr")
+
+        for (const [key, value] of Object.entries(teamStats)) {
+            if (key === "team_id") {
+                continue
+            } 
+
+            const cell = document.createElement("td");
+
+            cell.textContent = value;
+            row.appendChild(cell);
+        }
+
+        leagueTableHtml.appendChild(row)
+    }
+}
+
+//get the league table object from the backend
+async function getLeagueTable() {
+    try {
+        const respData = await fetch(`${apiBeginning}/results/leaguetable`)
+        let leagueTableList
+
+        if (respData.ok) {
+            leagueTableList = await respData.json()
+
+        } else {
+            throw "Something went wrong with the API request"
+        }
+
+        return leagueTableList
+
+    } catch (err) {
+        console.log(err);
+    }
+}
